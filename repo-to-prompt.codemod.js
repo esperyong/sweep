a968ab13ce5e6916a255e5f3d36838e40e40be33
clearwater <esperyong@gmail.com>
2024-03-12 10:24:51 +0800

    add pro

---
+++ repo-to-prompt.codemod.js
@@ -0,0 +1,9 @@
+// 修改 repo-to-prompt.codemod.js 写入下面的代码
+const lines = [];
+for (const file of selectedFiles) {
+    lines.push('<file path="' + file.path + '">')
+    lines.push(new TextDecoder().decode(await vscode.workspace.fs.readFile(file)))
+    lines.push('</file>')
+}
+await vscode.env.clipboard.writeText(lines.join('\n'))
+vscode.window.showInformationMessage('copied to clipboard')
